c1e5ed0c36263548198345d77acc9c67450ce354
SDB <db7734@naver.com>
2025-10-26 15:29:12 +0900

    완료 항목 basic 으로 이동

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import App from './App.jsx'
-import AppProfile from "./AppProfile.jsx";
-import AppCounter from "./AppCounter.jsx";
-import AppProducts from "./AppProducts.jsx";
+import AppProfile from "./basic/AppProfile.jsx";
+import AppCounter from "./basic/AppCounter.jsx";
+import AppProducts from "./basic/AppProducts.jsx";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <AppProducts/>
     </StrictMode>,
 )
